e448792e4d2de2fa5fe5197bebcfb93a86a4bf8d
Ganesh Pd. Joshi <abhinavjoshi11@gmail.com>
2017-02-19 12:08:15 +0545

    initialized list view

--- app/src/main/java/com/example/abhinav/jsonexample/MainActivity.java
+++ app/src/main/java/com/example/abhinav/jsonexample/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.abhinav.jsonexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG =MainActivity.class.getSimpleName();
+
+    private ListView lv;
+
+    //URL to get contacts JSON
+    private static String url = "https://jsonplaceholder.typicode.com/posts/";
+
+    ArrayList<HashMap<String ,String>> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contactList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.list);
+        new GetContacts().execute();
     }
 }
